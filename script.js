c982c4aa18c3971bef3050706a1e0a1f02b8a827
jitender4674 <gauravbudhwar9@gmail.com>
2025-01-06 14:32:56 +0530

    added functionalities

--- script.js
+++ script.js
@@ -4,6 +4,10 @@
 const dropMenu = document.getElementById('drop-menu-btn');
 const menuWindow = document.getElementById('menuWindow');
 const menuCloseBtn = document.querySelector('.menu-close-btn');
+const nav = document.querySelector('nav');
+const main = document.querySelector('main');
+// console.log(nav);
+
 
 // menu reveal
 dropMenu.addEventListener('click', () => {
@@ -15,6 +19,18 @@ menuCloseBtn.addEventListener('click', () => {
     menuWindow.classList.add('hidden');
 });
 
-// console.log(!menuWindow.classList.contains("hidden"))
+main.addEventListener('click', () => {
+    if (!menuWindow.classList.contains('hidden')) {
+        menuWindow.classList.add('hidden');
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        menuWindow.classList.add('hidden');
+    }
+})
+
+
 
 
